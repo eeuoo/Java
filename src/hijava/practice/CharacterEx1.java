f786571d17121decd6f95d585e1bb03220e5ca25
eeuoo <lee512joo@gmail.com>
2019-02-04 12:59:58 +0900

    2019-02-04

---
+++ src/hijava/practice/CharacterEx1.java
@@ -0,0 +1,19 @@
+package hijava.practice;
+
+public class CharacterEx1 {
+
+	public static void main(String[] args) {
+		char ch1 = 'A';
+		System.out.println(ch1);
+		System.out.println((int)ch1); // 문자에 해당하는 정수값이 출력됨
+		
+		char ch2 = 66;
+		System.out.println(ch2);
+		
+		int ch3 = 67;
+		System.out.println(ch3);
+		System.out.println((char)ch3);  // 정수값에 해당하는 문자가 출력됨
+
+	}
+
+}
---
+++ src/hijava/practice/OperationEx.java
@@ -0,0 +1,22 @@
+package hijava.practice;
+
+public class OperationEx {
+
+	public static void main(String[] args) {
+		int fatherAge = 45;
+		int motherAge = 47;
+		
+		char ch;
+		
+		ch = (fatherAge > motherAge) ? 'T' : 'F' ;
+		
+		char sh;
+		
+		sh = (fatherAge < motherAge) ? 'T' : 'F' ;
+		
+		System.out.println(ch);
+		System.out.println(sh);
+
+	}
+
+}
