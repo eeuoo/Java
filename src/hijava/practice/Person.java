e7f371ca34d88900f5e4645a6e89ed62d44e5931
eeuoo <lee512joo@gmail.com>
2019-02-06 22:27:26 +0900

    2019-02-06

---
+++ src/hijava/practice/Person.java
@@ -0,0 +1,32 @@
+package hijava.practice;
+
+public class Person {
+	String name;
+	float height;
+	int age;
+	
+	public Person() {}
+	
+	public Person(String pname) {
+		name = pname;
+	}
+	
+	public Person(String pname, float pheight, int page) {
+		name = pname;
+		height = pheight;
+		age = page;
+	}
+	
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", height=" + height + ", age=" + age + "]";
+	}
+	
+	public static void main(String[] args) {
+		Person Lee = new Person("lee", 170F , 33);
+		
+		System.out.println(Lee);
+	}
+
+}
+
