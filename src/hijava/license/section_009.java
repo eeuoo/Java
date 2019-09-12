5d57ade9a1ce855be4d80c13e222c346580b2a20
eeuoo <lee512joo@gmail.com>
2019-09-12 22:19:36 +0900

    2019-09-12

--- src/hijava/license/section_009.java
+++ src/hijava/license/section_009.java
@@ -2,14 +2,25 @@ package hijava.license;
 
 public class section_009 {
 
-	public static void main (String[] args) {
-		int i, j = 0;
-		for(i = 1; i < 8; i++) {
-			j = j + i;
+//	public static void main (String[] args) {
+//		int i, j = 0;
+//		for(i = 1; i < 8; i++) {
+//			j = j + i;
+//		}
+//		
+//		System.out.printf("%d, %d", i, j);
+//			
+//	}
+	
+	public static void main(String[] args) {
+		int a[] = new int[5];
+		int i;
+		for (i = 0; i < 5; i++) {
+			a[i] = i + 10;
 		}
-		
-		System.out.printf("%d, %d", i, j);
-			
+		for(i = 0; i < 5; i++) {
+			System.out.printf("%4d", a[i]);
+		}
 	}
 	
 	
