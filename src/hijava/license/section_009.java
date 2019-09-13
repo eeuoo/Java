25648c90fcdea0e282ae050aae4d97b3296d8f79
eeuoo <lee512joo@gmail.com>
2019-09-13 23:10:28 +0900

    2019-09-13

--- src/hijava/license/section_009.java
+++ src/hijava/license/section_009.java
@@ -13,14 +13,22 @@ public class section_009 {
 //	}
 	
 	public static void main(String[] args) {
-		int a[] = new int[5];
-		int i;
-		for (i = 0; i < 5; i++) {
-			a[i] = i + 10;
-		}
-		for(i = 0; i < 5; i++) {
-			System.out.printf("%4d", a[i]);
-		}
+//		int a[] = new int[5];
+//		int i;
+//		for (i = 0; i < 5; i++) {
+//			a[i] = i + 10;
+//		}
+//		for(i = 0; i < 5; i++) {
+//			System.out.printf("%4d", a[i]);
+//		}
+		
+		int[] a = {90, 100, 80, 70, 60, 50, 30};
+		int hap = 0;
+		float avg;
+		for(int i : a)
+			hap = hap + i;
+		avg = (float)hap / a.length;
+		System.out.printf("%4d, %4.2f", hap, avg);
 	}
 	
 	
